a3620752bf94c5eef559ac6bdc89295c3b2cf9da
VIVEK CHAUHAN <vivek698863@gmail.com>
2025-12-03 16:56:56 +0530

    Add files via upload

---
+++ logical_operation_ex3.py
@@ -0,0 +1,8 @@
+num1= int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+
+print(num1==num2 and num2<num1)
+
+print(num1==num2 or num2<num1)
+
+print(not(num1==num2 and num2<num1))
