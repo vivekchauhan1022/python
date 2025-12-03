4748669eaf90c2c01d4bf73f090a3e7008c7efd4
VIVEK CHAUHAN <vivek698863@gmail.com>
2025-12-03 16:59:04 +0530

    Add files via upload

---
+++ string_ex7.py
@@ -0,0 +1,15 @@
+name = "CHAUHAN VIVEKKUMAR"
+
+address = """t.k. hostel
+  sindhunagar,bhavnagar"""
+  
+print(name)
+
+print(name[0])
+print(name[1])
+print(name[4])
+print(name[0:5])
+print(name[4:14])
+print(name[14:])
+print(name+address)
+print(name*5)
